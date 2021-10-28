42287153b15b0953b36e5f0075ffadff9489a902
Ryan <ryanhill1128@gmail.com>
2021-10-28 09:09:02 -0500

    initial commit

---
+++ src/test/java/biz/rookware/springjokeapp/SpringJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.rookware.springjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
